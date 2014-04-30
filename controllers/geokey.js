0ab0ec6cc4d9c7c1883dec23c43847e0db53cd05
Jenny Kieffaber <jenny@igodigital.com>
2014-04-30 16:15:05 -0400

    bad syntax

--- controllers/geokey.js
+++ controllers/geokey.js
@@ -3,15 +3,13 @@
 var gk       = require('./models/geokey');
 var Geokey   = mongoose.model('Geokey', GeokeySchema);
 
-module.exports.controller = function(app) {
-
 function queryDB (res) {
     Geokey.find().exec(function(err, result) { 
         if (!err) { 
             res.write(JSON.stringify(result, undefined, 2));
             var query = Geokey.find();
-            query.exec(function(err, result) {
-        });
+            query.exec(function(err, result)
+        };
         } else {
             res.end('Error in query. ' + err)
         };
