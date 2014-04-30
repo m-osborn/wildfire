e2e687e364a17440080e58c88c7be1a0374ee93e
Jenny Kieffaber <jenny@igodigital.com>
2014-04-30 19:54:26 -0400

    opening the db conn

--- app.js
+++ app.js
@@ -137,16 +137,16 @@ http.createServer(app).listen(app.get('port'), function(){
 
 //Mongodb to hold jb activity configs
 mongoose.connect(MONGOHQ_URL);
-//mongoose.connect('mongodb://localhost/wildfire')
+// mongoose.connect('mongodb://localhost/wildfire')
 
 var db       = mongoose.connection;
 // var gk       = require('../models/geokey');
 // var Geokey   = mongoose.model('Geokey', GeokeySchema);
 
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function callback() {
-//     console.log("Connected to db");
-// });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+    console.log("Connected to db");
+});
 
 // //GET geokeys from db
 // app.get('/geokeys', function(req, res) {
