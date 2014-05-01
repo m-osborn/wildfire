b3e6575edfe4fb6f4bb46cf015e86628b1740a88
Jenny Kieffaber <jenny@igodigital.com>
2014-05-01 01:32:43 -0400

    reading

--- routes/geokey.js
+++ routes/geokey.js
@@ -10,23 +10,23 @@ exports.create = function( req, res ){
     console.log( 'req.body: ', req.body );
     console.log( 'inside routes.geokey.js create');
 
-    // new geokey({
-    //     map : req.body.map,
-    //     keywords: req.body.keywords
+    new geokey({
+        map : req.body.map,
+        keywords: req.body.keywords
 
-    // }).save(function(err){
-    //   if (err) return handleError(err);
-    // });
+    }).save(function(err){
+      if (err) return handleError(err);
+    });
 
     console.log( 'no errors!?');
 };
-
-console.log('outside of exports.create');
 
 exports.read = function (req, res){
     console.log( 'req.body: ', req.body );
     console.log( 'inside routes.geokey.js read ');
 
-    tmp = geokey.find()
-    console.log('tmp:   ', tmp)
+    tmp = geokey.find({}).exec(function(err, result) {
+      if (!err) return handleError(err);
+    });
+    console.log ('tmp:', tmp);
 };
