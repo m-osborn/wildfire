064c4873c65567ec15ecac16707b37e79e661001
Jenny Kieffaber <jenny@igodigital.com>
2014-04-30 23:36:41 -0400

    fixing stuff

--- routes/geokey.js
+++ routes/geokey.js
@@ -1,15 +1,15 @@
 'use strict';
 
 var mongoose = require('mongoose');
-// var gk       = require('../models/geokey');
-var geokey   = mongoose.model('Geokey', GeokeySchema);
+var gk       = require('../models/geokey');
+var geokey   = mongoose.model('Geokey', gk);
 
 // POST Handler for / route of Geocode (this is the create route).
 exports.create = function( req, res ){
 
     console.log( 'req.body: ', req.body );
 
-    new Geokey({
+    new geokey({
         map : req.body.map,
         keywords: req.body.keywords
     }).save(function( err, geokey, count ){
