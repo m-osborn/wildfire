037d38e503127f2a8bf113b37e1fd7e0312ecd91
Jenny Kieffaber <jenny@igodigital.com>
2014-04-30 23:00:59 -0400

    adding logs

--- routes/geokey.js
+++ routes/geokey.js
@@ -7,11 +7,12 @@ var Geokey = require('../models/geokey');
 exports.create = function( req, res ){
 
     console.log( 'req.body: ', req.body );
+    console.log( 'in the routes.geokey.js');
 
     new Geokey({
         map : req.body.map,
         keywords: req.body.keywords
     }).save(function( err, geokey, count ){
-    res.send( 200, 'create' );
+    res.send( 200, 'Create' );
     });
 };
