17bf7557d1de3bc65be2880e924c21e782c7dc41
Jenny Kieffaber <jenny@igodigital.com>
2014-04-30 22:35:52 -0400

    syntax

--- routes/geokey.js
+++ routes/geokey.js
@@ -2,19 +2,14 @@
 
 // POST Handler for / route of Geocode (this is the create route).
 
-exports.create = function( req, res ) {
+exports.create = function( req, res ){
 
     console.log( 'req.body: ', req.body );
-
-    exports.geokeys_data.push({
-        maps: req.body.map,
-        keywords: req.body.keywords
-    });
 
     new Geokey({
         map : tmp.map,
         keywords: tmp.keywords
     }).save(function( err, geokey, count ){
     res.send( 200, 'create' );
-});
+    });
 };
